5809897324b04e527401fcd909de178243913d81
Wafa Saefulhaq <saefulhaqwafa@gmail.com>
2023-09-19 11:22:26 +0700

    fib with dynamic programming

---
+++ src/recursive/fib.js
@@ -0,0 +1,43 @@
+/**
+ *
+ * @param {number} n
+ * @param {object} memo
+ * @returns {number}
+ */
+export function fib(n, memo = {}) {
+  // console.log("Hello n >> ", n);
+  if (n === 1 || n === 0) {
+    return n;
+  }
+  if (memo[n] === undefined) {
+    memo[n] = fib(n - 2, memo) + fib(n - 1, memo);
+  }
+  console.log("memo >> ", memo);
+  return memo[n];
+}
+
+/**
+ *
+ * @param {number} n
+ * @returns {number}
+ */
+function fibWithLoop(n) {
+  if (n === 0) {
+    return 0;
+  }
+  let a = 0;
+  let b = 1;
+  for (let i = 1; i < n; i++) {
+    let temp = a;
+    a = b;
+    b = temp + a;
+    console.log("index", i);
+    console.log("temp >> ", temp);
+    console.log("a >> ", a);
+    console.log("b >> ", b);
+  }
+  return b;
+}
+
+// console.log(fib(10));
+console.log(fibWithLoop(10));
---
+++ src/recursive/max.js
@@ -0,0 +1,29 @@
+/**
+ *
+ * @param {number[]} array
+ * @returns {number}
+ */
+export function max(array) {
+  console.log("hello");
+  if (array.length === 1) {
+    return array[0];
+  }
+
+  // O(N2)
+  // if (array[0] > max(array.slice(1, array.length))) {
+  //   return array[0];
+  // } else {
+  //   return max(array.slice(1, array.length));
+  // }
+
+  // O(2pangkatN)
+  let maxOfRemainder = max(array.slice(1, array.length));
+  console.log("maxOfRemainder", maxOfRemainder);
+  if (array[0] > maxOfRemainder) {
+    return array[0];
+  } else {
+    return maxOfRemainder;
+  }
+}
+
+console.log(max([1, 2, 3, 4]));
